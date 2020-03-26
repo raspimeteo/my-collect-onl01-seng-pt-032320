69437e52726789d1bc252cd02ebebde3f2b2f2f3
John Zonneveld <jjczon@gmail.com>
2020-03-26 19:59:40 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
 
+my_collect(collection) do |lang|
+    lang.upcase
+  end
 
+
+def my_collect
+    i=0
+    new_array = []
+    while i < collection.length
+        new_array << yield(collection[i])
+        i = i + 1
+    end
+    new_array
+end
